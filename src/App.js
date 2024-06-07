b33e6a2d826897dcf6e22cfaa1023945706cc8ea
NorhanAbdelkader <84976516+NorhanAbdelkader@users.noreply.github.com>
2024-06-07 16:39:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import Article from "./homeComponents/Article";
-import CreateArticle from "./homeComponents/CreateArticle";
-import { ArticleList } from "./homeComponents/Article";
-import HomePage from "./pages/Homepage";
-// import UserInfo from "./profileComponents/UserInfo";
-import UserProfile from "./pages/Profile";
 function App() {
   return (
-    <UserProfile/>
-    // <HomePage/>
-    // <Article/>
-    // <CreateArticle/>
+    <>
+    </>
   );
 }
 
